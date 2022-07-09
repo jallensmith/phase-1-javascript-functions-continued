0105394cf58414779ca122cd953a5d3c56b3c7ee
Jerome Allen-Smith <jeromeallen99@gmail.com>
2022-07-09 16:22:14 -0400

    commit tree 8d4aa2b5539ea257974ef392a8af1de8ebabd7a4

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork (activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+
+// function wrapAdjective(compliment){
+//     const '*' = "a hard worker"
+//     const '||' = "a dedicated programmer"
+//     return `You are ${compliment}`
+// }
+
+function wrapAdjective(compliment="*"){
+    return function(comp2="special"){
+        return `You are ${compliment}${comp2}${compliment}!`
+    }
+}
